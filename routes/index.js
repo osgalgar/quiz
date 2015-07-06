03d72e0e88272ec2116b309f93d6c9fd2cf48ddd
Oscar Gallego <osgalgar@gmail.com>
2015-07-06 16:51:52 +0200

    Mod6 Patrón MVC

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Créditos' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 

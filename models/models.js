231da205cf0ec87538c25ebb5f5c90d88827ca01
Oscar Gallego <osgalgar@gmail.com>
2015-07-14 12:04:16 +0200

    Lista de Preguntas

--- models/models.js
+++ models/models.js
@@ -33,14 +33,17 @@ var Quiz = sequelize.import(quiz_path);
 exports.Quiz = Quiz; // Exportar Definición de Tabla Quiz
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function() {
-  // succes(..) ejecuta el manejador una vez creada la tabla
-  Quiz.count().success(function (count) {
+sequelize.sync().then(function() {
+  // then(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().then(function (count) {
     if(count === 0) {   // la tabla se inicializa solo si está vacía
       Quiz.create({ pregunta:  'Cuál es la Capital de Italia?',
                     respuesta: 'Roma'
                   });
-      //.succes(function() {console.log('Base de Datos Inicializada');});
+      Quiz.create({ pregunta:  'Cuál es la Capital de Portugal?',
+                    respuesta: 'Lisboa'
+                  });
+    //.then(function(){console.log('Base de Datos Inicializada X')});
     };
   });
 });

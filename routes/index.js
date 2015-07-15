e8420e668bb1f49cbc2106921b2042989f9d6cee
Oscar Gallego <osgalgar@gmail.com>
2015-07-15 10:06:40 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,10 @@ router.get('/author', function(req, res) {
   res.render('author', { title: 'Créditos' });
 });
 
+// AutoLoad de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quizId
+
+// Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
